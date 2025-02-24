f6fabb65c8d59eecc05e7b52062d7c66aac4f056
Omid Talebi <omid.talebi@yahoo.com>
2025-02-24 14:14:55 +0330

    Add project files.

---
+++ ads.VirtualKeyboard/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ads.VirtualKeyboard
+{
+    public partial class Form1 : Form
+    {
+
+
+        private const int WS_EX_TOOLWINDOW = 0x00000080;
+        private const int WS_EX_NOACTIVATE = 0x08000000;
+        private const int WS_EX_TOPMOST = 0x00000008;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= (WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST);
+                cp.Parent = IntPtr.Zero; // Keep this line only if you used UserControl
+                cp.X = this.Location.X;
+                cp.Y = this.Location.Y;
+                return cp;
+
+                //return base.CreateParams;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("{" + (sender as Button).Tag.ToString() + "}");
+        }
+
+    }
+}
